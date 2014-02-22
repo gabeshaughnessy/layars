70164f74434516a7e1abb3b5469ef4d5b8cc0d17
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-02-21 19:08:21 -0800

    initial commit

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Pre-define connection to the MySQL database, please specify these fields based on your database configuration.*/
+  define('DBHOST', 'localhost');
+  define('DBDATA', 'gabesima_layar');
+  define('DBUSER', 'gabesima_layar');
+  define('DBPASS', 'G@briel1');
+?>
---
+++ images/sxsw-design_edge.js
@@ -0,0 +1,72 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+
+
+var resources = [
+];
+var symbols = {
+"stage": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+         dom: [
+         {
+            id:'sxsw-design',
+            type:'image',
+            rect:['0px','0px','600px','233px','auto','auto'],
+            fill:["rgba(0,0,0,0)",im+"sxsw-design.jpg",'0px','0px']
+         }],
+         symbolInstances: [
+
+         ]
+      },
+   states: {
+      "Base State": {
+         "${_Stage}": [
+            ["color", "background-color", 'rgba(255,255,255,1)'],
+            ["style", "width", '600px'],
+            ["style", "height", '233px'],
+            ["style", "overflow", 'hidden']
+         ],
+         "${_sxsw-design}": [
+            ["style", "top", '0px'],
+            ["style", "height", '233px'],
+            ["style", "left", '0px'],
+            ["style", "width", '600px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: true,
+         timeline: [
+         ]
+      }
+   }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-248098703");
